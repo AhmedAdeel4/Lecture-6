25fe6eab6b54177fa37d061a7eab0b24f64eeb5b
Your Name <you@example.com>
2024-06-30 10:23:54 +0200

    firstEdit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+
+//  npm install -g npm
+//  npm i -D react-router-dom@latest
+//  npm i sass
+//  npm i bootstrap@latest
+//  npm i axios
+//  npm i recoil
